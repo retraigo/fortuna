3c614dfeba24ec286cb17337dcd8effa7b8e057d
retraigo <nett@nekooftheabyss.xyz>
2022-04-06 13:31:03 +0530

    transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,13 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
- // loading: `${__dirname}/components/Global/Effects/PingEye.vue`,
+  // loading: `${__dirname}/components/Global/Effects/PingEye.vue`,
+  head: {
+    link: [
+      { rel: "stylesheet", href: "/transition.css" },
+    ],
+  },
   docs: {
-    primaryColor: '#ff00c3'
-  }
-})
+    primaryColor: "#ff00c3",
+  },
+});
