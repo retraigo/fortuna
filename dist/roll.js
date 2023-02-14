642afe5a2306c5e2b3a5221c4986b2382c422c4a
Neko Of The Abyss <psi@innocent.com>
2023-02-14 18:41:56 +0530

    bundle

--- dist/roll.js
+++ dist/roll.js
@@ -2,12 +2,16 @@
 // deno-lint-ignore-file
 // This code was bundled using `deno bundle` and it's not recommended to edit it manually
 
-function roll(choices, totalChance = 0) {
-    let total = totalChance;
+function roll(choices, chanceArrOrTotalChance = 0, totalChance = 0) {
+    const asArray = Array.isArray(chanceArrOrTotalChance);
+    let total = asArray ? totalChance : chanceArrOrTotalChance || 0;
+    if (typeof total !== "number") {
+        throw new TypeError(`Invalid type for total chance. Expected 'undefined' or 'number', got '${typeof total}'`);
+    }
     let i = 0;
-    if (totalChance === 0) {
+    if (total === 0) {
         while(i < choices.length){
-            total += choices[i].chance;
+            total += asArray ? chanceArrOrTotalChance[i] : choices[i].chance;
             i += 1;
         }
     }
@@ -15,12 +19,12 @@ function roll(choices, totalChance = 0) {
     let going = 0.0;
     i = 0;
     while(i < choices.length){
-        going += choices[i].chance;
+        going += asArray ? chanceArrOrTotalChance[i] : choices[i].chance;
         if (result < going) {
-            return choices[i].result;
+            return asArray ? choices[i] : choices[i].result;
         }
         i += 1;
     }
-    return choices[Math.floor(Math.random() * choices.length)].result;
+    return asArray ? choices[Math.floor(Math.random() * choices.length)] : choices[Math.floor(Math.random() * choices.length)].result;
 }
 export { roll as roll };
