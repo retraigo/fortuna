087da6ea34771ad031bef78029302c0d1156dc47
NeTT <nett@nett.moe>
2024-11-28 10:58:50 +0530

    optimize coins

--- src/coin.ts
+++ src/coin.ts
@@ -6,18 +6,24 @@
  * @returns Array of results. Returns "0" for head and "1" for tail.
  */
 export function unbiasedCoin(n = 1): ("0" | "1")[] {
-  if (n > 52) {
+  if (n > 62) {
     const res: ("0" | "1")[][] = [];
-    const times = Math.trunc(n / 52);
-    const extra = n % 52;
+    const times = Math.trunc(n / 62);
+    const extra = n % 62;
     for (let i = 0; i < times; i += 1) {
-      res.push(unbiasedCoin(52));
+      res.push(unbiasedCoin(62));
     }
     res.push(unbiasedCoin(extra));
     return res.flat();
   } else {
-    return Array.from(
-      Math.random().toString(2).slice(2, n + 2).padEnd(n, "0")
-    ) as ("0" | "1")[];
+    let rand = Math.random() * (1 << 62) - 1
+    const res = new Array(n);
+    for (let i = 0; i < n; i += 1) {
+      res[i] = (rand & 1) ? "T" : "H";
+      rand >>= 1;
+    }
+    return res;
   }
 }
+
+console.log(unbiasedCoin(200).join(""))
